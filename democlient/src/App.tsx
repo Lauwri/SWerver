dbd61f81351263f2500e94a85d537e2a3300bafd
Lauri <lauri.mattila@tuni.fi>
2020-02-07 17:54:53 +0200

    Separated server & react ts client

---
+++ democlient/src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+
+
+
+const App: React.FC = () => {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h3>SWerver simple client</h3>
+      </header>
+      
+      <div className="App-container">
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
